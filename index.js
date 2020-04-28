81c587bddbaf5ce19d2d7c15c6cf87f840076935
Ben Venker <bvemails@gmail.com>
2020-04-28 13:05:55 -0500

    finish queries and add get accounts endpoint

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const server = require("./api/server.js");
+const db = require("./data/dbConfig");
+const knex = require("knex");
 
 const PORT = process.env.PORT || 5000;
+
+server.get("/accounts", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .then((accounts) => res.status(200).json(accounts));
+});
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
